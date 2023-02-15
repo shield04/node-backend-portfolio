36b8958525ba827243367343b05f34e8e11bec03
Juan Losada <juand.losada77@gmail.com>
2023-02-15 00:11:18 -0500

    second commit

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var port = 3700;
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/portafolio')
+//mongoose.connect('mongodb://localhost:27017/portafolio')
+mongoose.connect('mongodb://127.0.0.1:27017/portafolio')
         .then(() => {
             console.log("Conexion a la base de datos establecida satisfactoriamente...");
 
