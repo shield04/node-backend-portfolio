582de05d9214e0ea75ec88a30593bc6b4e098656
Juan Losada <juand.losada77@gmail.com>
2023-02-23 02:30:07 -0500

    fix get images

--- routes/project.js
+++ routes/project.js
@@ -16,8 +16,8 @@ router.get('/projects', ProjectController.getProjects);
 router.put('/project/:id', ProjectController.updateProject);
 router.delete('/project/:id', ProjectController.deleteProject);
 router.post('/upload-image/:id', multipartMiddleware, ProjectController.uploadImage);
-//router.get('/:image', ProjectController.getImageFile);
-router.get('/get-image/:image', ProjectController.getImageFile);
+router.get('/:image', ProjectController.getImageFile);
+//router.get('/get-image/:image', ProjectController.getImageFile);
 
 
 
