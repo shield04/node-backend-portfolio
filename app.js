043081ae86f68a0b5461038adfdc1c600bad6b12
Juan Losada <juand.losada77@gmail.com>
2023-02-23 19:38:34 -0500

    add route

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://juandlosadaweb.com'); //, 'https://backenjuandlosada.lat/'
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Credentials', true); //Content-Type,
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
